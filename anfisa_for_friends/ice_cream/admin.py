6fa99b8406e644f24a41b738d193189c63936d09
kolokolnikovmb <kolokolnikovmb@yandex.ru>
2024-10-29 13:17:32 +0300

    Dev stage3

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Category, IceCream, Topping, Wrapper
+
+
+admin.site.empty_value_display = 'Не задано' 
+
+
+# Подготавливаем модель IceCream для вставки на страницу другой модели.
+class IceCreamInline(admin.StackedInline): #admin.TabularInline
+    model = IceCream
+    extra = 0
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',        
+    )
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    # В этом классе опишем все настройки, какие захотим.
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )    
+    search_fields = ('title',) 
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
 
 # Register your models here.
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+# Регистрируем новый класс: 
+# указываем, что для отображения админки модели IceCream
+# вместо стандартного класса нужно использовать класс IceCreamAdmin 
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Category, CategoryAdmin) 
